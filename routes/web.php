c527fc935e38c7d406feeb85f8e4b2baa0a9b221
Tomas <tomaspinedanaranjo@gmail.com>
2025-02-26 14:26:36 -0500

    Tutorial 3

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,10 @@ Route::get('/orders/list', 'App\Http\Controllers\OrderController@list')->name("o
 Route::post('/orders/save', 'App\Http\Controllers\OrderController@save')->name("order.save");
 Route::get('/orders/{id}', 'App\Http\Controllers\OrderController@show')->name("order.show");
 Route::get('/orders/{id}/delete', 'App\Http\Controllers\OrderController@delete')->name("order.delete");
+
+#Tutorial3
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll");
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.index");
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
